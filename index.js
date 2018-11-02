1d18e52cec6930eff6f05897d7a49acb96e2ee60
Daniel Kidon <dandankidon@gmail.com>
2018-11-02 20:55:46 +0200

    upgraded availability

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
 const userSchema = require('./forTests/userSchema');
 
-
 async function init(){
     let db = await mongoose.createConnection("mongodb://127.0.0.1:27017/test", {useNewUrlParser: true});
-    let userModel = DeepPopulateAllRefs(userSchema,'Users' ,db);
+    let userModel = DeepPopulateAllRefsFromSchema(userSchema,'Users' ,db);
     userModel.find({"name":"Daniel"})
     .exec((err,docs)=>{
         console.log(err);
-        console.log(docs[0]);
-        db.close();
-    })
+        console.log(docs[0].toObject());
+    }) 
 }
 
-function DeepPopulateAllRefs(schema, schemaName, conn){
+function DeepPopulateAllRefsFromSchema(schema, schemaName, conn){
     if(schema && schema.obj){
         Object.keys(schema.obj).forEach(key=>{
             if(schema.obj[key].ref){
